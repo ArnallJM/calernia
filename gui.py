6a3b280b48123dfb8610b374233e1472eb27d549
jarn3976 <jarn3976@uni.sydney.edu.au>
2021-08-29 16:32:06 +1000

    Functioning product with sliders!

--- gui.py
+++ gui.py
@@ -12,10 +12,15 @@ class GUI:
         self.player.storage.update()
         self.player.gui = self
 
-        self.current_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
-        self.target_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
-        self.strength_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
+        # self.current_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
+        # self.target_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
+        # self.strength_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
+
         self.attribute_str = np.empty(len(self.player.storage.attribute_names), tk.StringVar)
+
+        self.current_dou = np.empty(len(self.player.storage.attribute_names), tk.DoubleVar)
+        self.target_dou = np.empty(len(self.player.storage.attribute_names), tk.DoubleVar)
+        self.strength_dou = np.empty(len(self.player.storage.attribute_names), tk.DoubleVar)
 
         root.title("Calernia")
         self.mainframe = ttk.Frame(root, padding="3 3 12 12")
@@ -60,65 +65,119 @@ class GUI:
         save = ttk.Button(self.utility_column, text='Save', command=self.player.storage.save)
         save.grid(row=1, sticky='nswe')
 
+        # noot = tk.DoubleVar()
+        # noot.set(0)
+        #
+        # def joot(_):
+        #     # print(_)
+        #     noot.set(round(noot.get()))
+        #     print(noot.get())
+        #
+        # doot = ttk.Scale(self.utility_column, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=noot, command=joot)
+        # doot.grid(row=2, sticky='nswe')
+
     def when_track_changed(self):
         self.song_title.set(self.player.current_media.get_meta(vlc.Meta.Title))
         for i in range(len(self.player.current_song.attributes)):
-            self.current_str[i].set(str(self.player.current_song.attributes[i]))
+            self.current_dou[i].set(self.player.current_song.attributes[i])
+
+    # def alt_init_attribute_grid(self):
+    #     column = ttk.Frame(self.attribute_frame)
+    #     column['borderwidth'] = 2
+    #     column.grid(column=0, row=0, sticky='nwes')
+    #     ttk.Label(column, text='Current').grid(column=0, row=0, sticky='nsew')
+    #     ttk.Label(column, text='Target').grid(column=0, row=1, sticky='nsew')
+    #     ttk.Label(column, text='Strength').grid(column=0, row=2, sticky='nsew')
+    #
+    #     for i in range(len(self.current_dou)):
+    #         self.current_dou[i] = tk.DoubleVar()
+    #         self.target_dou[i] = tk.DoubleVar()
+    #         self.strength_dou[i] = tk.DoubleVar()
+    #         self.attribute_str[i] = tk.StringVar()
+    #         # self.attribute_str[i].set("doot")
+    #
+    #         self.current_dou[i].set(0)
+    #         self.target_dou[i].set(self.player.storage.target[i])
+    #         self.strength_dou[i].set(self.player.storage.strength[i])
+    #         self.attribute_str[i].set(self.player.storage.attribute_names[i])
+    #
+    #         column = ttk.Frame(self.attribute_frame)
+    #         column['borderwidth'] = 2
+    #         column.grid(column=i+1, row=0, sticky='nwes')
+    #
+    #         current_spinbox = ttk.Scale(column, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=self.current_dou[i], command=self.round_all_doubles)
+    #         target_spinbox = ttk.Scale(column, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=self.target_dou[i], command=self.round_all_doubles)
+    #         strength_spinbox = ttk.Scale(column, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=self.strength_dou[i], command=self.round_all_doubles)
+    #         name_label = ttk.Label(column, textvariable=self.attribute_str[i])
+    #
+    #         current_spinbox.grid(column=0, row=0, sticky='nesw')
+    #         # ttk.Separator(column, orient='horizontal').grid(column=0, row=1, sticky='ew')
+    #         target_spinbox.grid(column=0, row=1, sticky='nesw')
+    #         strength_spinbox.grid(column=0, row=2, sticky='nesw')
+    #         name_label.grid(column=0, row=3, sticky='nesw')
 
     def init_attribute_grid(self):
-        column = ttk.Frame(self.attribute_frame)
-        column['borderwidth'] = 2
-        column.grid(column=0, row=0, sticky='nwes')
-        ttk.Label(column, text='Current').grid(column=0, row=0, sticky='nsew')
-        ttk.Label(column, text='Target').grid(column=0, row=1, sticky='nsew')
-        ttk.Label(column, text='Strength').grid(column=0, row=2, sticky='nsew')
+        # column = ttk.Frame(self.attribute_frame)
+        # column['borderwidth'] = 2
+        # column.grid(column=0, row=0, sticky='nwes')
+        ttk.Label(self.attribute_frame, text='Current').grid(column=0, row=0, sticky='nsew')
+        ttk.Label(self.attribute_frame, text='Target').grid(column=0, row=1, sticky='nsew')
+        ttk.Label(self.attribute_frame, text='Strength').grid(column=0, row=2, sticky='nsew')
 
-        for i in range(len(self.current_str)):
-            self.current_str[i] = tk.StringVar()
-            self.target_str[i] = tk.StringVar()
-            self.strength_str[i] = tk.StringVar()
+        for i in range(len(self.current_dou)):
+            self.current_dou[i] = tk.DoubleVar()
+            self.target_dou[i] = tk.DoubleVar()
+            self.strength_dou[i] = tk.DoubleVar()
             self.attribute_str[i] = tk.StringVar()
             # self.attribute_str[i].set("doot")
 
-            self.current_str[i].set('0')
-            self.target_str[i].set(str(self.player.storage.target[i]))
-            self.strength_str[i].set(str(self.player.storage.strength[i]))
-            self.attribute_str[i].set(str(self.player.storage.attribute_names[i]))
+            self.current_dou[i].set(0)
+            self.target_dou[i].set(self.player.storage.target[i])
+            self.strength_dou[i].set(self.player.storage.strength[i])
+            self.attribute_str[i].set(self.player.storage.attribute_names[i])
 
-            column = ttk.Frame(self.attribute_frame)
-            column['borderwidth'] = 2
-            column.grid(column=i+1, row=0, sticky='nwes')
+            # column = ttk.Frame(self.attribute_frame)
+            # column['borderwidth'] = 2
+            # column.grid(column=i+1, row=0, sticky='nwes')
 
-            current_spinbox = ttk.Spinbox(column, from_=0, to=3, textvariable=self.current_str[i], command=self.toggle_confirm_button)
-            target_spinbox = ttk.Spinbox(column, from_=0, to=3, textvariable=self.target_str[i], command=self.toggle_confirm_button)
-            strength_spinbox = ttk.Spinbox(column, from_=0, to=3, textvariable=self.strength_str[i], command=self.toggle_confirm_button)
-            name_label = ttk.Label(column, textvariable=self.attribute_str[i])
+            current_spinbox = ttk.Scale(self.attribute_frame, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=self.current_dou[i], command=self.round_all_doubles)
+            target_spinbox = ttk.Scale(self.attribute_frame, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=self.target_dou[i], command=self.round_all_doubles)
+            strength_spinbox = ttk.Scale(self.attribute_frame, orient=tk.VERTICAL, length=100, from_=3, to=0, variable=self.strength_dou[i], command=self.round_all_doubles)
+            name_label = ttk.Label(self.attribute_frame, textvariable=self.attribute_str[i])
 
-            current_spinbox.grid(column=0, row=0, sticky='nesw')
+            current_spinbox.grid(column=i+1, row=0, sticky='nesw')
             # ttk.Separator(column, orient='horizontal').grid(column=0, row=1, sticky='ew')
-            target_spinbox.grid(column=0, row=1, sticky='nesw')
-            strength_spinbox.grid(column=0, row=2, sticky='nesw')
-            name_label.grid(column=0, row=3, sticky='nesw')
+            target_spinbox.grid(column=i+1, row=1, sticky='nesw')
+            strength_spinbox.grid(column=i+1, row=2, sticky='nesw')
+            name_label.grid(column=i+1, row=3, sticky='nesw')
+
+    def round_all_doubles(self, *args):
+        for i in range(len(self.current_dou)):
+            self.current_dou[i].set(round(self.current_dou[i].get()))
+            self.target_dou[i].set(round(self.target_dou[i].get()))
+            self.strength_dou[i].set(round(self.strength_dou[i].get()))
+        self.toggle_confirm_button()
 
     def update_current(self):
         if self.player.current_song is None:
             return
         # print([thing.get() for thing in self.current_str])
-        new_current = self.stringvar_to_int(self.current_str).tolist()  # TODO modify to accept arrays
+        new_current = self.doublevar_to_int(self.current_dou).tolist()  # TODO modify to accept arrays
         self.player.change_attributes(new_current, self.player.current_song)
-        self.player.print_current_info()
+        # self.player.print_current_info()
         # print(new_current)
 
     def update_target_strength(self):
-        new_strength = self.stringvar_to_int(self.strength_str)
-        new_target = self.stringvar_to_int(self.target_str)
+        new_strength = self.doublevar_to_int(self.strength_dou)
+        new_target = self.doublevar_to_int(self.target_dou)
         self.player.change_target(new_target, new_strength)
-        self.player.print_current_info()
+        # self.player.print_current_info()
 
     def update_all(self):
         self.update_current()
         self.update_target_strength()
         self.toggle_confirm_button()
+        self.player.print_current_info()
 
     @staticmethod
     def compare_attributes(a,b):
@@ -130,12 +189,12 @@ class GUI:
         return True
 
     def toggle_confirm_button(self):
-        if self.compare_attributes(self.player.storage.target, self.stringvar_to_int(self.target_str)):
-            if self.compare_attributes(self.player.storage.strength, self.stringvar_to_int(self.strength_str)):
+        if self.compare_attributes(self.player.storage.target, self.doublevar_to_int(self.target_dou)):
+            if self.compare_attributes(self.player.storage.strength, self.doublevar_to_int(self.strength_dou)):
                 if self.player.current_song is None:
                     self.confirm_button.state(['disabled'])
                     return
-                if self.compare_attributes(self.player.current_song.attributes, self.stringvar_to_int(self.current_str)):
+                if self.compare_attributes(self.player.current_song.attributes, self.doublevar_to_int(self.current_dou)):
                     self.confirm_button.state(['disabled'])
                     return
         self.confirm_button.state(['!disabled'])
@@ -154,6 +213,21 @@ class GUI:
             else:
                 output[i] = int(input[i].get())
         return output
+
+    @staticmethod
+    def doublevar_to_int(input):
+        if type(input) == tk.DoubleVar:
+            return int(round(input.get()))
+
+        output = np.empty(len(input), int)
+        for i in range(len(input)):
+            output[i] = int(round(input[i].get()))
+        return output
+
+
+class CustomScale(ttk.Scale):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
 
 
